d67352f7340e1111a510589ae0bb7a04c049453b
vagner-fonseca <vagner.cfonseca@gmail.com>
2020-12-30 14:56:45 -0300

    primeiro commit

---
+++ src/components/SliderData.js
@@ -0,0 +1,15 @@
+export const SliderData =[
+    {
+        image: 'https://images.unsplash.com/photo-1606038188414-ab55f710a8b0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1572250383938-ed8b3afe0b9a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1506795213373-430e921fe2ed?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1504639725590-34d0984388bd?ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80'
+    }
+
+];
